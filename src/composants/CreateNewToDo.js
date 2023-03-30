a033b93abad35e95da09188d4f17f03ee9f3b46d
Ya-2 <mazizyassine25@gmail.com>
2023-03-30 10:43:45 +0200

    ajout du bouton new Todo

--- src/composants/CreateNewToDo.js
+++ src/composants/CreateNewToDo.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Model';
 
 function CreateNewToDo(){
-
+    const [showModal, setShowModal] = useState(false) //showModal = false, setShowModal() pour le modifier
 
     return(
         <div className="CreateNewToDo">
-            <button></button>
+            <div className='btn_div'>
+            <button className='btn btn-primary' onClick={() => setShowModal(true)}>
+                New Todo
+            </button>
+            </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    Fenetre Ouverte !
+                </div>
+
+            </Modal>
         </div>
     )
 }

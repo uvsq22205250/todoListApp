0f96b9ab03114f017313491be802d41f50eafdc5
Ya-2 <mazizyassine25@gmail.com>
2023-04-05 00:15:42 +0200

    Fenetre create todo/ affichage todo

--- src/composants/Todos.js
+++ src/composants/Todos.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import Todo from "./Todo";
 import NextDays from "./NextDays";
+import 'bootstrap/dist/css/bootstrap.css';
 function Todos(){
 
+    //Données temporaires
+    const todos = [
+        {
+            id: '1',
+            text: "Play league of legend",
+            time: "10:00",
+            date: "03/04/2023",
+            checked: true,
+            project: 'personnal'
+        },
+        {
+            id: '2',
+            text: "anniversaire de tom",
+            time: "21:00",
+            date: "10/04/2023",
+            checked: false,
+            project: 'personnal'
 
+        }
+    ]
+    const selectedProject = "Today"
     return(
         <div className="Todos">
-            <Todo />
-            <NextDays />
+            <div className='selectedProject'>
+                {selectedProject}
+            </div>
+            <div className='listTodos'>
+                {
+                todos.map( todo =>
+                <Todo todo={todo} />)}
+            </div>
+            
 
         </div>
     )
